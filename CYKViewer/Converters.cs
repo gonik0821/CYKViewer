8d3dc27adb41dbebe32a343e1d090de4d47b5d55
gonik0821 <111680978+gonik0821@users.noreply.github.com>
2022-08-25 04:27:30 +0900

    Add files via upload

---
+++ CYKViewer/Converters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CYKViewer
+{
+    public class GameScreenSizeToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            GameScreenSize size = (GameScreenSize)value;
+            return size.Multiplier;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new GameScreenSize((double)value);
+        }
+    }
+
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isOpen = (bool)value;
+            return isOpen ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+            return visibility == Visibility.Visible;
+        }
+    }
+}
